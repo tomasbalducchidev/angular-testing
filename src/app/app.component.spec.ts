1dfb944e4ea0f3162bee949f7fd1c6bd5eb1fd97
Tomas Balducchi <tomasbalducchi@gmail.com>
2025-05-14 16:57:38 -0300

    test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,10 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-import { HttpClient } from '@angular/common/http';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
-      providers: [HttpClient],
     }).compileComponents();
   });
 
